f153613de397fb1974822ce91ebd9fe638a7868b
Tengman-never <Tengman204@hdu.edu.cn>
2020-09-20 14:49:38 +0800

    添加cpp文件

---
+++ GitTest/sushu.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+	for (i = 2; i <= 100; i++)
+	{
+		for (j = 2; j <= i-1 ;j++)
+		{
+			if (i%j == 0)//能整除则不是素数
+			{
+				break;	
+			}	
+		}
+		if (j > i / 2)
+		{
+			cout << i << " ";
+		}			
+	}
+	system("pause");
+	return 0;
+
+
+}
