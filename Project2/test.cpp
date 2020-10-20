cd5549a964d12608a37323c6e7e17ac8395d13bb
Tengman-never <Tengman204@hdu.edu.cn>
2020-10-20 10:08:02 +0800

    1

--- Project2/test.cpp
+++ Project2/test.cpp
@@ -2,7 +2,7 @@
 #include<opencv2/opencv.hpp>
 using namespace cv;
 using namespace std;
-/*
+
 void test1()
 {
 	    cv::Mat img = imread("E:/C++demo/Project2/04/1.jpg", 0);
@@ -23,11 +23,11 @@ void test1()
 		//morphologyEx(src, dst, 2, kernel, Point(-1, -1));
 		//imshow("open", dst);
 		/*---------闭运算--------*/
-		//morphologyEx(src, dst, 3, kernel, Point(-1, -1));
-		//imshow("close", dst);
-		//waitKey(0); 
-//}
-/*
+		morphologyEx(src, dst, 3, kernel, Point(-1, -1));
+		imshow("close", dst);
+		waitKey(0); 
+}
+
 RNG rng(123);
 void test2()
 {
@@ -95,18 +95,13 @@ void test2()
 
 	waitKey(0);
 }
-*/
+
 
 void test3()
 {	
-	cv::Mat src_binary;
-	cv::Mat image_gray;
-
-	cv::Mat src = imread("E:/C++demo/Project2/04/2.jpg");
-	cvtColor(src, image_gray, COLOR_BGR2GRAY);
-
-	threshold(image_gray, src_binary, 100, 255, THRESH_BINARY);
-	imshow("二值化后", src_binary);
+	Mat src_binary;
+	Mat image_gray;
+	Mat src = imread("E:/C++demo/Project2/04/2.jpg");
 	// 对图像进行所有像素用 （255- 像素值）
 	Mat image;
 	src.copyTo(image);
@@ -123,7 +118,7 @@ void test3()
 	// 每个像素点的每个通道255取反（0-255（黑-白））
 	uchar* p1;
 	uchar* p2;
-	for (int row = 0; row < rows; row++) 
+	for (int row = 0; row < rows; row++)
 	{
 		p1 = src.ptr<uchar>(row);// 获取像素指针
 		p2 = image.ptr<uchar>(row);
@@ -133,59 +128,37 @@ void test3()
 			p2++;
 			p1++;
 		}
-	}
-	imshow("取反后的图像", image);
 
-	Mat image_Ostu;//大津法分割后的图像
-	Mat image_Open;//开运算后的图像
-	//大津法分割图像
-	threshold(image, image_Ostu, 0, 255, THRESH_OTSU);
-	imshow("大津法后的图像", image_Ostu);
-	/*Mat kernel = getStructuringElement(MORPH_RECT, Size(11, 11));
-	morphologyEx(image_Ostu, image_Open, 2, kernel, Point(-1, -1), 1);//开运算
+	}
+	cvtColor(image, image_gray, COLOR_BGR2GRAY);
+	threshold(image_gray, src_binary, 100, 255, THRESH_BINARY);
+	imshow("二值化后", src_binary);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(20, 20), Point(-1, -1));
+	Mat dstImage_Open;
+	morphologyEx(src_binary, dstImage_Open, MORPH_OPEN, kernel, Point(-1, -1));
+	imshow("开运算处理后", dstImage_Open);
+
 	Mat labels;
 	Mat stats;
 	Mat centroids;
-	int num = connectedComponentsWithStats(image_Open, labels, stats, centroids, 8, CV_32S);
-	imshow("开运算后的图像", image_Open);
-	/*
+	int num = connectedComponentsWithStats(dstImage_Open, labels, stats, centroids, 8, CV_32S);
+	cout << "原点个数："<<num - 1 << endl;
+
+	std::vector<cv::Vec3b> colors(num);
+	colors[0] = cv::Vec3b(0, 0, 0);//背景黑色
+
 	//输出连通域信息
 	for (int i = 0; i < num; i++)
 	{
 		//各个连通域的统计信息保存在stasMat中
-		Vec2d pt = centroids.at<Vec2d>(i, 0);
 
-		int x = stats.at<int>(i, CC_STAT_LEFT);
-		int y = stats.at<int>(i, CC_STAT_TOP);
-		int width = stats.at<int>(i, CC_STAT_WIDTH);
-		int height = stats.at<int>(i, CC_STAT_HEIGHT);
-		int area = stats.at<int>(i, CC_STAT_AREA);
-
-		cout << x << "," << y << "," << width << "," << height << "," << area << endl;
-		circle(image_Open , Point(pt[0], pt[1]), 2, Scalar(0, 0, 255), -1, 8, 0);
-		rectangle(image_Open, Rect(x, y, width, height), Scalar(255, 255, 255), 1, 8, 0);
-	}
-	cout << "连通域个数为 " << num - 1 << endl;//-1,num包括背景
-
-	vector<Vec3f> circles;
-	HoughCircles(image, circles, CV_HOUGH_GRADIENT, 1.5, 10, 200, 100, 0, 0);
-	for (size_t i = 0; i < circles.size(); i++)
-	{
-		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
-		//绘制圆心
-		circle(image, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-		//绘制圆轮廓
-		circle(image, center, radius, Scalar(155, 50, 255), 3, 8, 0);
-
+		cout << "connected Components NO. " << i << endl;
+		cout << "pixels = " << stats.at<int>(i, 4) << endl;
+		cout << "width = " << stats.at<int>(i, 2) << endl;
+		cout << "height = " << stats.at<int>(i, 3) << endl;
+		cout << endl;
 	}
 
-	count(image_Open);
-	imshow("标记后的图像", image_Open);
-	std::vector<cv::Vec3b> colors(num);
-	colors[0] = cv::Vec3b(0, 0, 0);//背景黑色
-
-*/
 	waitKey(0);
 }
 
