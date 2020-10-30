f076311f38e2faa5c96d520d9a0a120387ecc9cd
Tengman-never <Tengman204@hdu.edu.cn>
2020-10-30 10:06:55 +0800

    1

--- Project2/test.cpp
+++ Project2/test.cpp
@@ -9,7 +9,7 @@ int test1()
 	    double scale = 0.5;
 		if (!cap.isOpened())
 		{
-			std::cout << "不能打开视频文件" << std::endl;
+			cout << "不能打开视频文件" << endl;
 			return -1;
 		}
 		Mat dstImage;
@@ -18,7 +18,7 @@ int test1()
 			Mat frame;
 			cap >> frame;
 			imshow("src", frame);
-			medianBlur(frame, dstImage, 11);
+			medianBlur(frame, dstImage, 5);
 			imshow("中值滤波", dstImage);
 			waitKey(30); 
 		}
@@ -32,7 +32,7 @@ int test2()
 	double scale = 0.5;
 	if (!cap.isOpened())
 	{
-		std::cout << "不能打开视频文件" << std::endl;
+		cout << "不能打开视频文件" << endl;
 		return -1;
 	}
 	Mat dstImage;
@@ -40,7 +40,8 @@ int test2()
 	{
 		Mat frame;
 		cap >> frame;
-		blur(frame, dstImage, Size(5, 5), Point(-1, -1), BORDER_DEFAULT);
+		imshow("frame", frame);
+		blur(frame, dstImage, Size(5,5), Point(-1, -1), BORDER_DEFAULT);
 		imshow("均值滤波", dstImage);
 		waitKey(30);
 	}
@@ -53,7 +54,7 @@ int test3()
 	double scale = 0.5;
 	if (!cap.isOpened())
 	{
-		std::cout << "不能打开视频文件" << std::endl;
+		cout << "不能打开视频文件" << endl;
 		return -1;
 	}
 	Mat dstImage;
@@ -61,7 +62,8 @@ int test3()
 	{
 		Mat frame;
 		cap >> frame;
-		GaussianBlur(frame, dstImage, Size(5, 5), 5, 0, BORDER_DEFAULT);
+		imshow("frame", frame);
+		GaussianBlur(frame, dstImage, Size(5, 5), 0, 0, BORDER_DEFAULT);
 		imshow("均值滤波", dstImage);
 		waitKey(30);
 	}
@@ -71,10 +73,9 @@ int test3()
 int test4()
 {
 	VideoCapture cap(0);
-	double scale = 0.5;
 	if (!cap.isOpened())
 	{
-		std::cout << "不能打开视频文件" << std::endl;
+		cout << "不能打开视频文件" << endl;
 		return -1;
 	}
 	Mat dstImage;
@@ -82,8 +83,17 @@ int test4()
 	{
 		Mat frame;
 		cap >> frame;
-		GaussianBlur(frame, dstImage, Size(5, 5), 5, 0, BORDER_DEFAULT);
-		imshow("均值滤波", dstImage);
+		imshow("frame", frame);
+		/*
+		Mat dx;
+		Mat dy;
+		Sobel(frame, dx, -1, 1, 0, 3);
+		Sobel(frame, dy, -1, 0, 1, 3);
+		imshow("dx", dx);
+		imshow("dy", dy);*/
+		//Sobel(frame, dstImage, -1, 1, 0, 3);//x方向
+		Sobel(frame, dstImage, -1, 0, 1, 3);//y方向
+		imshow("边缘提取", dstImage);
 		waitKey(30);
 	}
 	return 0;
@@ -93,6 +103,11 @@ int test5()
 {
 	VideoCapture cap(0);
 	double scale = 0.5;
+	if (!cap.isOpened())
+	{
+		cout << "不能打开视频文件" << endl;
+		return -1;
+	}
 	//0-180肤色
 	double i_minH = 0;
 	double i_maxH = 20;
@@ -102,39 +117,41 @@ int test5()
 	//0-255 
 	double i_minV = 55;
 	double i_maxV = 255;
-	if (!cap.isOpened())
-	{
-		std::cout << "不能打开视频文件" << std::endl;
-		return -1;
-	}
 	while (1)
 	{
 		Mat frame;
-		cap >> frame;
 		Mat hsvMat;
-		Mat detectMat;	
+		Mat detectMat;
+
+		cap >> frame;		
 		Size ResImgSiz = Size(frame.cols*scale, frame.rows*scale);
 		Mat rFrame = Mat(ResImgSiz, frame.type());
 		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
-
+		//将原图转化为HSV图像
 		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
 		rFrame.copyTo(detectMat);
 
-		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
+		inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
 
-		imshow("whie:in the range", detectMat);
-		imshow("frame", rFrame);
+		Mat image; 
+		Mat dst;
+		GaussianBlur(rFrame, image, Size(5, 5), 3, 3);		
+		rFrame.copyTo(dst);
+		image.copyTo(dst, detectMat);
+
+		imshow("detectMat", detectMat);//原图
+		imshow("rFrame", image);//高斯滤波后
+		imshow("dst", dst);
 		waitKey(30);
-
 	}
 }
 int main()
 {
-	test1();
+	//test1();
 	//test2();
 	//test3();
 	//test4();
-	//test5();
+	test5();
 	system("pause");
 	return 0;
 }
