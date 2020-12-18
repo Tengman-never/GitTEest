961378767eb030517ebc76deb4832d66d24e162b
Tengman-never <Tengman204@hdu.edu.cn>
2020-12-18 20:03:11 +0800

    1

--- Project2/test.cpp
+++ Project2/test.cpp
@@ -4,128 +4,110 @@ using namespace cv;
 using namespace std;
 int test1()
 {
-	//Mat grayMat,binMat;
-	Mat frame = imread("E:/C++demo/image/image1.jpg");
-	//cvtColor(frame, grayMat, COLOR_BGR2GRAY);
-	//threshold(grayMat,binMat,100, 255, THRESH_OTSU);
-	float LUT[256] = { 0 };
-	for (int i = 0; i < 256; ++i)
+	VideoCapture cap;
+	cap.open(0);
+	int cnt = 0;
+	Mat bgMat,subMat,resMat;
+	while (1)
 	{
-		for (int j = 0; j < 256; j++)
-		{
-			int m = 0;
-		    LUT[i] = frame.at<uchar>(i,j);
-		}
-		float s = (float)(i / 255.0);
-		float gamma = 0.55;
-		float op = pow(s, gamma);
-		LUT[i] = op * 255;
-		cout << "像素值为：" << i << "=" << LUT[i] << endl;
+		Mat frame;
+		cap >> frame;
+		cvtColor(frame, frame, COLOR_BGR2GRAY);
 
-	}
-	Mat dst;
-	frame.copyTo(dst);
-	//if (dst.channels() == 1) 
-	//{
-	//	cv::MatIterator_<uchar> it = dst.begin<uchar>();
-	//	cv::MatIterator_<uchar> it_end = dst.end<uchar>();
-	//	for (; it != it_end; ++it) 
-	//	{
-	//		*it = LUT[(*it)];
-	//	}
-	//}
-	//else
-	//{
-		cv::MatIterator_<cv::Vec3b> it = dst.begin<cv::Vec3b>();
-		cv::MatIterator_<cv::Vec3b> it_end = dst.end<cv::Vec3b>();
-		for (; it != it_end; ++it) 
+		if (cnt == 0)
 		{
-			(*it)[0] = LUT[(*it)[0]];
-			(*it)[1] = LUT[(*it)[1]];
-			(*it)[2] = LUT[(*it)[2]];
+			frame.copyTo(bgMat);
+			cnt++;
 		}
-	//}
-	imshow("frame", frame);
-	//imshow("grayMat", grayMat);
-	//imshow("binMat", binMat);
-	imshow("dst", dst);
-	waitKey(0);
+		else
+		{
+			absdiff(frame, bgMat, subMat);
+			threshold(subMat, resMat, 100, 255, CV_THRESH_OTSU);
+			imshow("subMat", subMat);
+			imshow("resMat", resMat);
+			waitKey(30);
+		}
+	}
+
 	return 0;		
 }
 int test2()
 {
-	cv::Mat src = imread("E:/C++demo/image/image2.jpg");
-	std::vector<cv::Mat> channels;
-	cv::split(src, channels);
-	cv::Mat B = channels.at(0);
-	cv::Mat G = channels.at(1);
-	cv::Mat R = channels.at(2);
-	Mat B1, G1, R1;
-
-	equalizeHist(B, B1);
-	equalizeHist(G, G1);
-	equalizeHist(R, R1);
-
-	Mat dst;
-	vector<Mat> mbgr(3);
-	mbgr[0] = B1;
-	mbgr[1] = G1;
-	mbgr[2] = R1;
-	merge(mbgr, dst);
-
-	//imshow("B", B);
-	//imshow("G", G);
-	//imshow("R", R);
-
-	imshow("src", src);
-	imshow("dst", dst);
-
-	waitKey(1000);
-	return 0;
-
-}
-//gamma矫正
-int test3()
-{
-	Mat frame = imread("E:/C++demo/image/image3.jpg");
-
-	float LUT[256] = { 0 };
-	for (int i = 0; i < 256; ++i)
+	VideoCapture cap;
+	cap.open(0);
+	vector<Mat> image;
+	int cnt = 0;
+	Mat mean(480,640, CV_8UC1);
+	Mat var(480, 640, CV_32FC1);
+	int h = 640;
+	int w = 480;
+	bool haveModel = false;
+	while (1)
 	{
-		for (int j = 0; j < 256; j++)
+		Mat frame;
+		cap >> frame;
+		cvtColor(frame, frame, COLOR_BGR2GRAY);
+		threshold(frame, frame, 100, 255, THRESH_OTSU);
+		if (cnt < 20)
 		{
-			int m = 0;
-			LUT[i] = frame.at<uchar>(i, j);
+			image.push_back(frame);
 		}
-		float s = (float)(i / 255.0);
-		float gamma = 0.43;
-		float op = pow(s, gamma);
-		LUT[i] = op * 255;
-		cout << "像素值为：" << i << "=" << LUT[i] << endl;
+		else
+		{
+			//没有高斯模型，则计算
+			if (!haveModel)
+			{
+				//计算均值和方差
+				for (cnt = 0; cnt < 20; cnt++)
+				{
+					//Mat Th = var;
+					for (int j = 0; j < h; j++)
+					{
+						for (int i = 0; i < w; i++)
+						{
+							for (int m = 0; m < 20; m++)
+							{
+								mean.at<uchar>(i, j) += image[m].at<uchar>(i, j);
+							}
+							mean.at<uchar>(i, j) /= 20;
+							for (int n = 0; n < 20; n++)
+							{
+								var.at<float>(i, j) = pow(float(image[n].at<uchar>(i, j) - mean.at<uchar>(i, j)), 2);
+							}
+							var.at<float>(i, j) /= 20;
+
+						}
+					}
+				}
+			}
+			//模型计算完成，置true,下次不再计算
+			haveModel = true;
+
+			Mat Th = var;
+			for (int j = 0; j < h; j++)
+			{
+				for (int i = 0; i < w; i++)
+				{
+					if (frame.at<uchar>(i, j) - mean.at<uchar>(i, j) > Th.at<uchar>(i, j))
+					{
+						frame.at<uchar>(i, j) = 255;
+					}
+					else
+					{
+						frame.at<uchar>(i, j) = 0;
+					}
+				}
+			}
+		}
+		imshow("frame", frame);
+		waitKey(30);
 	}
-
-	Mat dst;
-	frame.copyTo(dst);
-
-    cv::MatIterator_<cv::Vec3b> it = dst.begin<cv::Vec3b>();
-	cv::MatIterator_<cv::Vec3b> it_end = dst.end<cv::Vec3b>();
-	for (; it != it_end; ++it) 
-	{
-		(*it)[0] = LUT[(*it)[0]];
-		(*it)[1] = LUT[(*it)[1]];
-		(*it)[2] = LUT[(*it)[2]];
-	}
-
-	imshow("frame", frame);
-	imshow("dst", dst);
-	waitKey(0);
 	return 0;
 }
 int main()
 {
 	//test1();
 	test2();
-	//test3();
 	waitKey(0);	
 	system("pause");
 	return 0;
