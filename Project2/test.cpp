b107123ad14013cdbda9614182e469178ebc8501
Tengman-never <Tengman204@hdu.edu.cn>
2021-01-08 08:16:39 +0800

    1

--- Project2/test.cpp
+++ Project2/test.cpp
@@ -1,98 +1,90 @@
 ﻿#include<iostream>
 #include<opencv2/opencv.hpp>
-using namespace cv;
 using namespace std;
+using namespace cv;
+void on_mouse(int EVENT, int x, int y, int flags, void* userdata);
 RNG rng(123);
-int main()
+void main()
 {
-	Mat src_binary;
-	Mat image_gray;
-	Mat src = imread("E:/C++demo/image/final.jpg");
-	// 对图像进行所有像素用 （255- 像素值）
-	Mat image;//取反后的图像
-	src.copyTo(image);
-	// 获取图像宽、高
-	int channels = src.channels();
-	int rows = src.rows; //高---行
-	int col = src.cols;//宽---列
-	int cols = src.cols * channels;
-	if (src.isContinuous())
+	VideoCapture cap("C:/Users/滕曼/Desktop/video.mp4");
+	double scale = 1;
+	//0-180
+	double i_minH = 8;
+	double i_maxH = 10;
+	//0-255
+	double i_minS = 43;
+	double i_maxS = 255;
+	//0-255 
+	double i_minV = 46;
+	double i_maxV = 255;
+
+	while (1)
 	{
-		cols *= rows;
-		rows = 1;
-	}
-	// 每个像素点的每个通道255取反（0-255（黑-白））
-	uchar* p1;
-	uchar* p2;
-	for (int row = 0; row < rows; row++)
-	{
-		p1 = src.ptr<uchar>(row);// 获取像素指针
-		p2 = image.ptr<uchar>(row);
-		for (int col = 0; col < cols; col++)
+		Mat frame;
+		Mat hsvMat;
+		Mat detectMat;
+		Mat image;
+		cap >> frame;
+		if (frame.empty())
+			break;
+		GaussianBlur(frame,frame, Size(5,5),0,0);
+		//ksize必须是大于1的奇数
+		//medianBlur(frame,frame,5);
+		//imshow("frame",frame);
+
+		Size ResImgSiz = Size(frame.cols*scale, frame.rows*scale);
+		Mat rFrame = Mat(ResImgSiz, frame.type());
+		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
+		//RGB转化为HSV  
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+		rFrame.copyTo(detectMat);
+		//主要是将在两个阈值内的像素值设置为白色（255），而不在阈值区间内的像素值设置为黑色（0）
+		//输出的detectMat是一幅二值化之后的图像。
+		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
+		//灰度化
+		threshold(detectMat, detectMat, 100, 255, THRESH_OTSU);
+		//setMouseCallback("【display】", on_mouse, &detectMat);
+
+		vector<Vec4i>hierarchy;
+		std::vector<std::vector<Point>> contours;
+		findContours(detectMat, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+		vector<Rect> boundRect(contours.size());
+		for (int i = 0; i < contours.size(); i++)
 		{
-			*p2 = 255 - *p1; // 取反
-			p2++;
-			p1++;
+			RotatedRect rbox = minAreaRect(contours[i]);
+			float width = (float)rbox.size.width;
+			float height = (float)rbox.size.height;
+			float radio = width / height;
+			if (width * height > 280 && width > 30)
+			{
+				boundRect[i] = boundingRect(Mat(contours[i]));
+				rectangle(rFrame, boundRect[i].tl(), boundRect[i].br(), (0, 0, 255), 2, 8, 0);
+			}
 		}
+	
 
+		/*imshow("frame", frame);*/
+		imshow("whie:in the range", detectMat);
+		imshow("rFrame", rFrame);
+		waitKey(30);
 	}
-	imshow("取反后的图像", image);
-	cvtColor(image, image_gray, COLOR_BGR2GRAY);//灰度化
-	threshold(image_gray, src_binary, 100, 255, THRESH_BINARY);//二值化
-	//imshow("二值化后", src_binary);
-
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(10, 10), Point(-1, -1));
-	Mat dstImage_Open;
-	morphologyEx(src_binary, dstImage_Open, MORPH_OPEN, kernel, Point(-1, -1));
-	imshow("开运算处理后", dstImage_Open);
-
-	Mat labels;
-	Mat stats;
-	Mat centroids;
-	int num = connectedComponentsWithStats(dstImage_Open, labels, stats, centroids, 8, CV_32S);
-	cout << "焊点个数：" << num - 1 << endl;
-
-	//使用不同的颜色标记连通域
-	//std::vector<cv::Vec3b> colors(num);
-	//colors[0] = cv::Vec3b(0, 0, 0);//背景黑色
-
-	//for (int i = 1; i < num; i++)
-	//{
-	//	colors[i] = Vec3b(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-	//}
-	//Mat dst = Mat::zeros(src.size(), src.type());
-	//int w = src.cols;
-	//int h = src.rows;
-	//for (int row = 0; row < h; row++)
-	//{
-	//	for (int col = 0; col < w; col++)
-	//	{
-	//		int label = labels.at<int>(row, col);
-	//		if (label == 0) continue;
-	//		dst.at<Vec3b>(row, col) = colors[label];
-	//	}
-	//}
-	//imshow("连通域标记", dst);
-
-	//标记连通域
-
-	for (int i = 1; i < num; i++)
+}
+void on_mouse(int EVENT, int x, int y, int flags, void* userdata)
+{
+	Mat hh;
+	hh = *(Mat*)userdata;
+	Point p(x, y);
+	switch (EVENT)
 	{
-		Vec2d pt = centroids.at<Vec2d>(i, 0);//二维数组
-		int x = stats.at<int>(i, CC_STAT_LEFT);
-		int y = stats.at<int>(i, CC_STAT_TOP);
-		int width = stats.at<int>(i, CC_STAT_WIDTH);
-		int height = stats.at<int>(i, CC_STAT_HEIGHT);
-		int area = stats.at<int>(i, CC_STAT_AREA);
-		//cout << "连通域中心的矩阵" << endl;
-		//cout << pt[0] << "," << pt[1] << endl;
-		//cout << "状态矩阵" << endl;
-		//cout << x << "," << y << "," << width << "," << height << "," << area << endl;
+	case EVENT_LBUTTONDOWN:
+	{
 
-		rectangle(src, Rect(x, y, width, height), Scalar(0, 255, 255), 3, 8, 0);
+		printf("b=%d\t", hh.at<Vec3b>(p)[0]);
+		printf("g=%d\t", hh.at<Vec3b>(p)[1]);
+		printf("r=%d\n", hh.at<Vec3b>(p)[2]);
+		circle(hh, p, 2, Scalar(255), 3);
 	}
-	imshow("连通域标记图像", src);
+	break;
 
-	waitKey(0);
-	system("pause");
+	}
 }
