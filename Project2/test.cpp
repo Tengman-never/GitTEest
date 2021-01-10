1d098a0b9bcd93acdb58555837071866c010bd4a
Tengman-never <Tengman204@hdu.edu.cn>
2021-01-10 09:44:10 +0800

    1

--- Project2/test.cpp
+++ Project2/test.cpp
@@ -115,17 +115,106 @@ void segColor()
 	waitKey(0);
 
 }
-////特效
-void test3()
+//特效
+//融合算法
+
+int test3()
 {
-	Mat src = imread("E:/C++demo/image/15.jpg");
-	Mat mask = Mat::zeros(src.size(), CV_8UC1);
-	createMaskByKmeans(src, mask);
+	VideoCapture capture, capture_TV;
+	capture.open("E:/C++demo/小猫.mp4");
+	capture_TV.open("E:/C++demo/小鞠.mp4");
+	double scale = 0.5;
+	VideoWriter writer;
+	writer.open("./my1.flv", writer.fourcc('F', 'L', 'V', '1'), 30, Size(1366, 768), true);//CAP_OPENCV_MJPEG
+
+	Mat frame;
+	Mat hsv, green_mask, green_mask_bin;
+	vector<Vec4i>hierarchy;
+	std::vector<std::vector<Point>> contours;
+	Mat result, background, mask;
+
+	while (capture.read(frame))
+	{
+		//寻找绿幕
+		cvtColor(frame, hsv, COLOR_BGR2HSV);
+		inRange(hsv, Scalar(35, 43, 46), Scalar(77, 255, 255), green_mask);
+		threshold(green_mask, green_mask_bin, 100, 255, THRESH_OTSU);
+		findContours(green_mask_bin, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE, Point(0, 0));
+
+		vector <vector<Point>>::iterator iter = contours.begin();
+		for (; iter != contours.end();)
+		{
+			double g_dConArea = contourArea(*iter);
+			if (g_dConArea < 290000 || g_dConArea > 320000)
+			{
+				iter = contours.erase(iter);
+			}
+			else
+			{
+				++iter;
+			}
+		}
+		if (contours.size() == 0)
+		{
+			frame.copyTo(result);
+
+			imshow("ImageShow2", frame);
+			imshow("ImageShow", result);
+			writer.write(result);
+		}
+
+		capture_TV.read(background);
+		drawContours(mask, contours, -1, Scalar(255), CV_FILLED);
+		
+		for (int i = 0; i < (int)contours.size(); i++)
+		{
+			Rect rect1 = boundingRect(Mat(contours[i]));
+			resize(background, background, rect1.size());
+			int h = frame.rows;
+			int w = frame.cols;
+			//int bg_row = 0;
+			//dims = frame.channels();
+			for (int row = 0; row < h; row++)
+			{
+				uchar* current = frame.ptr<uchar>(row);
+				//uchar* bgrow = background.ptr<uchar>(bg_row);
+				uchar* maskrow = mask.ptr<uchar>(row);
+				uchar* targetrow = result.ptr<uchar>(row);
+				int m;
+				for (int col = 0; col < w; col++)
+				{
+					m = *maskrow++;
+					if (m == 255)
+					{
+						*targetrow++ = background.at<Vec3b>(row - rect1.y, col - rect1.x)[0];
+						*targetrow++ = background.at<Vec3b>(row - rect1.y, col - rect1.x)[1];
+						*targetrow++ = background.at<Vec3b>(row - rect1.y, col - rect1.x)[2];
+						current += 3;
+
+					}
+					else if (m == 0)
+					{
+						*targetrow++ = *current++;
+						*targetrow++ = *current++;
+						*targetrow++ = *current++;
+					}
+				}
+			}
+		}
+		waitKey(2);
+		//imshow("ImageShow2", frame);
+		//imshow("ImageShow1", background);
+		imshow("ImageShow", result);
+		writer.write(result);
+	}
+
+	return 0;
+
 }
 void main()
 {
 	//test1();
-	segColor();
-
+	//segColor();
+	test3();
 }
 
